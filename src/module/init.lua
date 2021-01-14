d000157c8fc73329ca7ab2763ab19e4ff78f2df3
VerdommeMan <VerdommeMan1717@gmail.com>
2021-01-14 05:39:18 +0100

    add support for negative numbers

--- src/module/init.lua
+++ src/module/init.lua
@@ -1,4 +1,8 @@
-local prefixes = {"K", "M", "G", "T", "P", "E", "Z", "Y", [0] = "", [-1] = "m", [-2] = "μ", [-3] = "n", [-4] = "p", [-5] = "f", [-6] = "a", [-7] = "z", [-8] = "y"}
+local scales = {
+    SI = {"K", "M", "G", "T", "P", "E", "Z", "Y", [0] = "", [-1] = "m", [-2] = "μ", [-3] = "n", [-4] = "p", [-5] = "f", [-6] = "a", [-7] = "z", [-8] = "y"},
+
+
+}
 
 local function isNumber(arg)
     return type(arg) == "number"
@@ -8,9 +12,9 @@ local function toReadable(self, ...)
     local returns = {}
     for _, number in ipairs({...}) do
         if isNumber(number) then
-            local index = math.floor(math.log10(number) / 3)    
+            local index = math.floor(math.log10(math.abs(number)) / 3)  
             if index > -9 and index < 9 then
-                table.insert(returns, (string.gsub(string.format("%#." .. self.precision .. "f",  number / 10 ^ (index * 3)), "%.0*$", "")) .. prefixes[index])
+                table.insert(returns, (string.gsub(string.format("%#." .. self.precision .. "f",  number / 10 ^ (index * 3)), "%.0*$", "")) .. scales[self.scale][index] )
             else
                 table.insert(returns, string.format("%g", number))
             end
@@ -21,4 +25,4 @@ local function toReadable(self, ...)
     return unpack(returns)
 end
 
-return setmetatable({precision = 3}, {__call = toReadable})
+return setmetatable({precision = 3, scale = "SI", unit = ""}, {__call = toReadable})
